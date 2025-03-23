0165839d4b4761dc8042bc96c56593c79fc73beb
Paulo Lopes <paulosousasancheslopes@gmail.com>
2025-03-23 20:40:26 -0300

    Adjustments

--- top10calculadoras/Calculadora.java
+++ top10calculadoras/Calculadora.java
@@ -8,12 +8,7 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        Operadores soma = Factory.factory("Soma");
-        Operadores subt = Factory.factory("Subtracao");
-        Operadores mult = Factory.factory("Multiplicacao");
-        Operadores divi = Factory.factory("Divisao");
-
-        System.out.println("Bem vindo a revolucionário dispositivo chamado calculadora!");
+        System.out.println("===== CALCULADORA =====\n");
 
         while (true) {
             System.out.println("Insisra dois números para obter um novo resultado!");
@@ -23,23 +18,21 @@ public class Calculadora {
             System.out.println("Digite o segundo número: ");
             double b = s.nextDouble();
 
-            System.out.println("\nAgora escolha uma operação:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair\n: ");
-            int op = s.nextInt();
+            System.out.println("\nAgora escolha uma operação:\n- Soma\n- Subtração\n- Multiplicação\n- Divisão\n- Sair\n: ");
+            String op = s.nextLine();
 
-            if (op == 1) {
-                System.out.println("\nO resultado da soma é: " + soma.calcular(a, b));
-            } else if (op == 2) {
-                System.out.println("\nO resultado da subtração é: " + subt.calcular(a, b));
-            } else if (op == 3) {
-                System.out.println("\nO resultado da multiplicação é: " + mult.calcular(a, b));
-            } else if (op == 4) {
-                System.out.println("\nO resultado da divisão é: " + divi.calcular(a, b));
-            } else if (op == 5) {
-                System.out.println("\nAté mais!");
+            if (op.equals("Sair")) {
+                System.out.println("Até mais!");
                 break;
+            }
+
+            Operadores operacao = Factory.factory(op);
+
+            if (operacao == null) {
+                System.out.println("Operação inválida!");
+                continue;
             } else {
-                System.out.println("\nOpção inválida!");
-                s.close();
+                System.out.println("Resultado: " + operacao.calcular(a, b));
             }
         }
     }
--- top10calculadoras/models/Divisao.java
+++ top10calculadoras/models/Divisao.java
@@ -3,11 +3,9 @@ package top10calculadoras.models;
 public class Divisao implements Operadores {
     @Override
     public double calcular(double a, double b) {
-        try {
-            return a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Erro: " + e.getMessage());
-            return 0;
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida.");
         }
+        return a / b;
     }
 }
